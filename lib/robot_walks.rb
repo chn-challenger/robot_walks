a95a04c9dccd392761d4493e2272348637e6aa9b
Joe Zhou <chn.challenger@gmail.com>
2017-04-26 01:24:14 +0100

    ready for refactor

--- lib/robot_walks.rb
+++ lib/robot_walks.rb
@@ -15,18 +15,13 @@ module RobotWalks
     (Math::sin(54.to_f / 180 * Math::PI)) / (Math::sin(72.to_f / 180 * Math::PI)) * r
   end
 
-  def left_arc_center(r,a)
+  def arc_center(r,a)
     result_r = (Math::sin(54.to_f / 180 * Math::PI)) / (Math::sin(72.to_f / 180 * Math::PI)) * r
     result_a = (a-54)%360
     result_a = result_a.to_f / 180 * Math::PI
     x_coord = result_r*Math::cos(result_a)
     y_coord = result_r*Math::sin(result_a)
     [x_coord,y_coord]
-  end
-
-  def draw_left_arc_center(r,a)
-    coords = left_arc_center(r,a)
-    "\\node[circle,draw=black, fill=black,inner sep=0pt, minimum size=3pt] at (#{coords[0]},#{coords[1]}) {};\n"
   end
 
   def draw_arc(starting_a,ending_a,radius,center)
@@ -41,8 +36,12 @@ module RobotWalks
     draw_arc(starting_a,ending_a,radius,center)
   end
 
-  def draw_left(r,a)
-    draw_polar(r,a) + draw_left_arc_center(r,a) + draw_left_arc(r,a)
+  # def draw_left(r_1,a_1)
+  #   draw_polar(r,a) + draw_left_arc_center(r,a) + draw_left_arc(r,a)
+  # end
+
+  def draw_left(r_1,a_1,r_2,a_2)
+    draw_polar_a_b(r_1,a_1,r_2,a_2) + draw_left_arc_center(r_1,a_1,r_2,a_2) + draw_left_arc(r_1,a_1,r_2,a_2)
   end
 
   def polar_to_xy(r,a)
@@ -51,5 +50,30 @@ module RobotWalks
     y_coord = r*Math::sin(a)
   end
 
+  def draw_polar_a_b(r_1,a_1,r_2,a_2)
+    a_coords = to_xy(r_1,a_1)
+    ab_coords = to_xy(r_2,a_2)
+    b_coords = [a_coords[0]+ab_coords[0],a_coords[1]+ab_coords[1]]
+    "\\draw[->,black,line width=2pt,smooth](#{a_coords[0]},#{a_coords[1]})--(#{b_coords[0]},#{b_coords[1]}){};\n"
+  end
+
+  def left_arc_center(r_1,a_1,r_2,a_2)
+    a_coords = to_xy(r_1,a_1)
+    relative_center = arc_center(r_2,a_2)
+    [a_coords[0]+relative_center[0],a_coords[1]+relative_center[1]]
+  end
+
+  def draw_left_arc_center(r_1,a_1,r_2,a_2)
+    coords = left_arc_center(r_1,a_1,r_2,a_2)
+    "\\node[circle,draw=black, fill=black,inner sep=0pt, minimum size=3pt] at (#{coords[0]},#{coords[1]}) {};\n"
+  end
+
+  def draw_left_arc(r_1,a_1,r_2,a_2)
+    starting_a = 180 + (a_2 - 54) - 72
+    ending_a = starting_a + 72
+    radius = arc_radius(r_2,a_2)
+    center = left_arc_center(r_1,a_1,r_2,a_2)
+    draw_arc(starting_a,ending_a,radius,center)
+  end
 
 end
--- spec/robot_walks_spec.rb
+++ spec/robot_walks_spec.rb
@@ -6,19 +6,15 @@ describe RobotWalks do
 
   describe '#somemethod' do
     it 'check settings' do
-      # puts dummy_class.draw_polar(5,144)
-      # puts dummy_class.draw_left_arc_center(5,144)
-      # puts dummy_class.draw_left_arc(5,144)
-      # res = "\\begin{tikzpicture}\n" + dummy_class.draw_left(5,72) + "\\end{tikzpicture}\n"
-      # res += "\\begin{tikzpicture}\n" + dummy_class.draw_left(5,144) + "\\end{tikzpicture}\n"
-      # Clipboard.copy res
+      res = "\\begin{tikzpicture}\n" +
+        dummy_class.draw_left(5,72,5,72) +
+        dummy_class.draw_left(5,72,5,144) +
+        dummy_class.draw_left(0,0,5,72) + 
+        # dummy_class.draw_polar_a_b(5,72,5,72) +
+        # dummy_class.draw_left_arc_center(5,72,5,72) +
+        # dummy_class.draw_left_arc(5,72,5,72) +
+        "\\end{tikzpicture}\n"
 
-      res = "\\begin{tikzpicture}\n" + dummy_class.draw_left(5,72)
-      res += dummy_class.draw_left(5,144)
-      res += dummy_class.draw_left(5,216)
-      res += dummy_class.draw_left(5,288)
-      res += dummy_class.draw_left(5,0)  + "\\end{tikzpicture}\n"
-      # res += dummy_class.draw_left(5,144) + "\\end{tikzpicture}\n"
       Clipboard.copy res
     end
   end
