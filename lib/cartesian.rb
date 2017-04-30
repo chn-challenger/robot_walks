70ac1f02a14cf485f72bc24b4f9b91b10ed0ea90
Joe Zhou <chn.challenger@gmail.com>
2017-04-30 20:59:09 +0100

    Cartesian

---
+++ lib/cartesian.rb
@@ -0,0 +1,17 @@
+class CartesianCoordinate
+  attr_accessor :x_coord, :y_coord
+
+  def initialize(x_coord,y_coord)
+    @x_coord = x_coord
+    @y_coord = y_coord
+  end
+
+  def to_polar
+
+  end
+
+  def length
+
+  end
+
+end
--- lib/polar.rb
+++ lib/polar.rb
@@ -1,8 +1,17 @@
-class PolarCoordinates
+class PolarCoordinate
   attr_accessor :radius, :angle
 
   def initialize(radius,angle)
     @radius = radius
     @angle = angle
   end
+
+  def plus(vector_b)
+
+  end
+
+  def to_cartesian
+
+  end
+
 end
